7a3786bbbc2ee67c951c4e1063b7153745c1a293
Radhabhumi Pangkanont <122279464+9gatsu28nichi@users.noreply.github.com>
2026-02-22 17:09:54 +0700

    Update CoreBankSystem.java

--- CoreBankSystem.java
+++ CoreBankSystem.java
@@ -4,39 +4,47 @@ public class CoreBankSystem implements BankService {
 
     @Override
     public boolean deposit(BankAccount account, double amount) {
-        if (amount <= 0) {
-            createTransaction("DEPOSIT", amount, "FAILED");
-            return false;
-        }
+        synchronized (account) {
+            if (amount <= 0) {
+                createTransaction("DEPOSIT", amount, "FAILED");
+                return false;
+            }
 
-        account.updateBalance(amount);
-        createTransaction("DEPOSIT", amount, "SUCCESS");
-        return true;
+            account.updateBalance(amount);
+            createTransaction("DEPOSIT", amount, "SUCCESS");
+            return true;
+        }
     }
 
     @Override
     public boolean withdraw(BankAccount account, double amount) {
-        if (amount <= 0 || account.getBalance() < amount) {
-            createTransaction("WITHDRAW", amount, "FAILED");
-            return false;
-        }
+        synchronized (account) {
+            if (amount <= 0 || account.getBalance() < amount) {
+                createTransaction("WITHDRAW", amount, "FAILED");
+                return false;
+            }
 
-        account.updateBalance(-amount);
-        createTransaction("WITHDRAW", amount, "SUCCESS");
-        return true;
+            account.updateBalance(-amount);
+            createTransaction("WITHDRAW", amount, "SUCCESS");
+            return true;
+        }
     }
 
     @Override
     public boolean transfer(BankAccount from, BankAccount to, double amount) {
-        if (amount <= 0 || from.getBalance() < amount) {
-            createTransaction("TRANSFER", amount, "FAILED");
-            return false;
-        }
+        synchronized (from) {
+            synchronized (to) {
+                if (amount <= 0 || from.getBalance() < amount) {
+                    createTransaction("TRANSFER", amount, "FAILED");
+                    return false;
+                }
 
-        from.updateBalance(-amount);
-        to.updateBalance(amount);
-        createTransaction("TRANSFER", amount, "SUCCESS");
-        return true;
+                from.updateBalance(-amount);
+                to.updateBalance(amount);
+                createTransaction("TRANSFER", amount, "SUCCESS");
+                return true;
+            }
+        }
     }
 
     private void createTransaction(String type, double amount, String status) {
